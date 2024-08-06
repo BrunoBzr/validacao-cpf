6dc2dcfea50a23dd06300ddaccd380deda10c091
Bruno Pereira <81038402+BrunoBzr@users.noreply.github.com>
2024-08-06 08:57:05 -0300

    Update validadorDeCPF.c

--- validadorDeCPF.c
+++ validadorDeCPF.c
@@ -1,15 +1,22 @@
 #include <stdio.h>
 
 void main(){
-	int cpf[11], i;
+	int cpf[11], i, soma = 0, mult = 10;
 	
 	/*informando o cpf*/
-	for(i = 0; i < 11; i++){
+	for(i=0; i<11; i++){
 		int d;
 		printf("Informe o %d digito: ", i+1);
 		scanf("%d", &d);
 		cpf[i] = d;
 	}
+	
+	for(i=0; i<9; i++){
+		soma += cpf[i] * mult;
+		mult--;
+		printf("Soma: %d\n", soma);
+	}
+	
 	
 	/*imprimindo o cpf*/
 	for(i = 0; i < 11; i++){
