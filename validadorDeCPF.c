6a3d010d6d5649d7081cc0cee3106fb207be42e4
Bruno Pereira <81038402+BrunoBzr@users.noreply.github.com>
2024-08-06 10:01:15 -0300

    Verificando o primeiro digito

--- validadorDeCPF.c
+++ validadorDeCPF.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
 void main(){
-	int cpf[11], i, soma = 0, mult = 10;
-	
+	int cpf[11], i, primeiro_digito, segundo_digito;
+	int soma = 0, mult = 10, resto_divisao = 0;
 	/*informando o cpf*/
 	for(i=0; i<11; i++){
 		int d;
@@ -11,12 +11,20 @@ void main(){
 		cpf[i] = d;
 	}
 	
+	
+	/*Verificando o primeiro digito*/
 	for(i=0; i<9; i++){
 		soma += cpf[i] * mult;
 		mult--;
-		printf("Soma: %d\n", soma);
 	}
-	
+	resto_divisao = soma%11;
+		if(resto_divisao < 2){
+			primeiro_digito = 0;
+		}else{
+			primeiro_digito = 11 - resto_divisao;
+		}
+		
+	printf("Primeiro digito: %d\n", primeiro_digito);
 	
 	/*imprimindo o cpf*/
 	for(i = 0; i < 11; i++){
